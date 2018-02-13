007eca138a49ae88a4754b9a903fb5f6e4b4af35
ConfusedSky <masamaedae@gmail.com>
2018-02-12 17:51:03 -0800

    Initial Commit

---
+++ Timewarp/Assets/CubeSpawner.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeSpawner : MonoBehaviour {
+
+    public GameObject prefab;
+    public int frequency = 4;
+
+    float timeSince = 0;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		if(Time.time > timeSince + (float)1/frequency)
+        {
+            timeSince = Time.time;
+            Instantiate(prefab, transform.position, Quaternion.identity, gameObject.transform);
+        }
+	}
+}
---
+++ Timewarp/Assets/TimeManagement.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.Characters.FirstPerson;
+
+public class TimeManagement : MonoBehaviour {
+
+    public static TimeManagement instance;
+    public FirstPersonController player;
+    public float defaultTime = .05f;
+
+    private float playerScale = 0;
+
+    void Awake()
+    {
+        instance = this;
+    }
+
+	// Use this for initialization
+	void Start ()
+    {
+
+	}
+
+    void Update()
+    {
+        if(Input.GetButtonDown("Fire1"))
+        {
+            if(Time.timeScale == defaultTime)
+                SetTimeScale(1f);
+            else
+                SetTimeScale(defaultTime);
+        }
+    }
+
+    public void SetTimeScale(float scale)
+    {
+        playerScale = Time.timeScale / scale;
+        Time.timeScale = scale;
+        Time.fixedDeltaTime = Time.timeScale * .02f;
+        Invoke("UpdatePlayer", .05f*scale);
+    }
+
+    private void UpdatePlayer()
+    {
+        player.m_WalkSpeed *= playerScale;
+        player.m_RunSpeed *= playerScale;
+        player.m_JumpSpeed *= playerScale;
+        player.m_GravityMultiplier *= playerScale * playerScale;
+    }
+}
