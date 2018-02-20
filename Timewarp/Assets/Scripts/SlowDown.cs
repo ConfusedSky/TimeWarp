3af3ad5c5aacf3fd7772b230a0ca169ae0b8160c
ConfusedSky <masamaedae@gmail.com>
2018-02-20 13:35:06 -0800

    Added Whacker

---
+++ Timewarp/Assets/Scripts/SlowDown.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Collider))]
+public class SlowDown : MonoBehaviour {
+    void OnTriggerEnter()
+    {
+        TimeManagement.instance.SetTimeScale(1f);
+    }
+}
---
+++ Timewarp/Assets/Scripts/Whacker.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Whacker : MonoBehaviour {
+    public Vector3 force;
+
+	void OnTriggerStay(Collider other)
+    {
+        if (Time.timeScale > .9f)
+        {
+            Debug.Log(other.gameObject);
+            other.GetComponent<CharacterController>().Move(force*Time.deltaTime);
+        }
+    }
+
+}
