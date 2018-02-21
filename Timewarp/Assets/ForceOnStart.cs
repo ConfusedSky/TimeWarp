59a6cb595620439b22a37c5ca7ed89ce72eb0ef3
ConfusedSky <masamaedae@gmail.com>
2018-02-21 13:26:25 -0800

    Fleshed out more of the level and stuff

---
+++ Timewarp/Assets/ForceOnStart.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class ForceOnStart : MonoBehaviour {
+
+    public Vector3 force;
+
+	// Use this for initialization
+	void Start () {
+        GetComponent<Rigidbody>().AddForce(force, ForceMode.Impulse);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
